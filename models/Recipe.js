c9530341374987ba8730c6dca96b6d88a0e449b1
Gurpreet4949 <complansandhu17@gmail.com>
2025-01-27 16:44:51 -0500

    recipe model created

---
+++ models/Recipe.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+    recipeName: {
+        type: String,
+        required: true
+    },
+    ingredients: {
+        type: [String],
+        required: true
+    },
+    cookingTime: {
+        type: Number, // in minutes
+        required: true
+    },
+    difficulty: {
+        type: String,
+        enum: ['Easy', 'Medium', 'Hard'],
+        required: true
+    },
+    cuisine: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    photoLink: {
+        type: String,
+        required: true
+    },
+    averageRating: {
+        type: Number,
+        min: 0,
+        max: 5,
+        default: 0
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Recipe', recipeSchema); 
