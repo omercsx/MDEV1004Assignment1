8381a0632fe77808faaf145d10052af4e37807df
Gurpreet4949 <complansandhu17@gmail.com>
2025-01-27 16:45:11 -0500

    mongodb connected

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe");
 const app = express();
 
 const mongoURI = "mongodb+srv://user3:user3@cluster0.wljfh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
+// Connect to MongoDB
+mongoose.connect(mongoURI)
+	.then(() => console.log('Connected to MongoDB'))
+	.catch(err => console.error('Could not connect to MongoDB:', err));
+
 app.get("/", (req, res) => {
 	res.send("Hello World");
+});
+
+// Endpoint to get top 20 recipes sorted by average rating
+app.get("/api/recipes", async (req, res) => {
+	try {
+		const recipes = await Recipe.find()
+			.sort({ averageRating: -1 })
+			.limit(20);
+		res.json(recipes);
+	} catch (error) {
+		res.status(500).json({ message: "Error fetching recipes", error: error.message });
+	}
 });
 
 app.listen(3000, () => {
